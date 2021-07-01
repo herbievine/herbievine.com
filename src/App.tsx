36393118f5b0cd4449b30387f8af0e7adf55c650
Herbie Vine <vineherbie@gmail.com>
2021-07-01 18:58:51 +0100

    changed socials and skills

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
 import {
-    default as FullPageScrollingEffect,
-    fullpageApi,
-    Origin,
-    Destination,
-    Direction
+  default as FullPageScrollingEffect,
+  fullpageApi,
+  Origin,
+  Destination,
+  Direction
 } from '@fullpage/react-fullpage'
 import Landing from './components/Landing'
 import Biography from './components/Biography'
@@ -13,54 +13,54 @@ import Socials from './components/Socials'
 import Navigation from './components/Navigation'
 
 function App() {
-    const [scroll, setScroll] = useState<string>('')
-    const [api, setApi] = useState<fullpageApi>()
-    const [navigation, setNavigation] = useState<
-        [Origin, Destination, Direction] | []
-    >([])
+  const [scroll, setScroll] = useState<string>('')
+  const [api, setApi] = useState<fullpageApi>()
+  const [navigation, setNavigation] = useState<
+    [Origin, Destination, Direction] | []
+  >([])
 
-    const handleOnLeave = (
-        origin: Origin,
-        destination: Destination,
-        direction: Direction
-    ) => {
-        setNavigation([origin, destination, direction])
-        scrollEffect(direction)
-    }
+  const handleOnLeave = (
+    origin: Origin,
+    destination: Destination,
+    direction: Direction
+  ) => {
+    setNavigation([origin, destination, direction])
+    scrollEffect(direction)
+  }
 
-    const scrollEffect = (direction: 'up' | 'down') => {
-        setScroll(direction)
+  const scrollEffect = (direction: 'up' | 'down') => {
+    setScroll(direction)
 
-        setTimeout(() => {
-            setScroll('')
-        }, 800)
-    }
+    setTimeout(() => {
+      setScroll('')
+    }, 800)
+  }
 
-    return (
-        <div className="app bg-gray-100 dark:bg-gray-900">
-            <Navigation api={api} onLeave={navigation} />
-            <div className={scroll ? `scroll--${scroll}` : ''}>
-                <FullPageScrollingEffect
-                    onLeave={(origin, destination, direction) =>
-                        handleOnLeave(origin, destination, direction)
-                    }
-                    scrollOverflow={true}
-                    scrollingSpeed={800}
-                    render={({ fullpageApi }) => {
-                        setApi(fullpageApi)
-                        return (
-                            <div>
-                                <Landing api={fullpageApi} />
-                                <Biography api={fullpageApi} />
-                                <Skills api={fullpageApi} />
-                                <Socials api={fullpageApi} />
-                            </div>
-                        )
-                    }}
-                />
-            </div>
-        </div>
-    )
+  return (
+    <div className="app bg-gray-100 dark:bg-gray-900">
+      <Navigation api={api} onLeave={navigation} />
+      <div className={scroll ? `scroll--${scroll}` : ''}>
+        <FullPageScrollingEffect
+          onLeave={(origin, destination, direction) =>
+            handleOnLeave(origin, destination, direction)
+          }
+          scrollOverflow={true}
+          scrollingSpeed={800}
+          render={({ fullpageApi }) => {
+            setApi(fullpageApi)
+            return (
+              <div>
+                <Landing api={fullpageApi} />
+                <Biography api={fullpageApi} />
+                <Skills api={fullpageApi} />
+                <Socials api={fullpageApi} />
+              </div>
+            )
+          }}
+        />
+      </div>
+    </div>
+  )
 }
 
 export default App
--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,19 +1,19 @@
 import { fullpageApi } from '@fullpage/react-fullpage'
 
 interface Landing {
-    api: fullpageApi
+  api: fullpageApi
 }
 
 function Landing(props: Landing) {
-    return (
-        <div className="section w-screen flex justify-start items-center ml-8 sm:ml-12 md:ml-32 text-4xl sm:text-6xl md:text-8xl dark:text-gray-100">
-            <h1 className="max-w-3/4">
-                Hi,
-                <br />
-                I'm <span className="animated-underline">Herbie</span>
-            </h1>
-        </div>
-    )
+  return (
+    <div className="section w-screen flex justify-start items-center ml-8 sm:ml-12 md:ml-32 text-4xl sm:text-6xl md:text-8xl dark:text-gray-100">
+      <h1 className="max-w-3/4">
+        Hi,
+        <br />
+        I'm <span className="animated-underline">Herbie</span>
+      </h1>
+    </div>
+  )
 }
 
 export default Landing
