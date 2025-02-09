0d2bfc806fa4a64f6c5711fdc07b0ff6600c6df2
Herbie Vine <me@herbievine.com>
2025-02-09 16:03:28 +0100

    feat: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 import { defaultLang, languages } from "./src/i18n/ui";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://herbievine.com",
-  integrations: [tailwind()],
+  trailingSlash: "never",
+  integrations: [tailwind(), sitemap()],
   i18n: {
     locales: Object.keys(languages),
     defaultLocale: defaultLang,
